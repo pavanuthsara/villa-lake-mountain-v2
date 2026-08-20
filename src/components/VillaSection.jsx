4b7296d9c4b8e273ce33ab06f5c5cee370704ddf
Pavan Uthsara <uthsarapavan@gmail.com>
2026-08-20 13:24:43 +0530

    change width of villa section cards

--- src/components/VillaSection.jsx
+++ src/components/VillaSection.jsx
@@ -62,7 +62,8 @@ const VillaSection = () => {
             </div>
           </div>
           {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8"> */}
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-8 justify-items-center max-w-4xl mx-auto">
+          {/* <div className="grid grid-cols-1 md:grid-cols-3 gap-8 justify-items-center max-w-4xl mx-auto"> */}
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 max-w-6xl mx-auto">
             {villas.map((villa) => (
               <VillaCard
                 key={villa.id}
